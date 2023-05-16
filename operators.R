2668b0292a77a26f5bcd767f1e635748919cd0e3
K Mahesh <mahesh0352002@gmail.com>
2023-05-16 12:44:10 +0530

    commit

---
+++ operators.R
@@ -0,0 +1,25 @@
+#arithmetic operators(+,-,*,/)
+quater_1_sales <- 35657.98
+quater_2_sales <- 43810.55
+midyear_sales <- quater_1_sales + quater_2_sales
+midyear_sales
+yearend_sales <- midyear_sales * 2
+yearend_sales
+
+#logical operator(and,or,not)
+
+#AND (&)
+TRUE & FALSE
+FALSE & FALSE
+FALSE & TRUE
+
+x<-10
+x>3 & x<12
+
+#OR(|)
+x>3 | x<9
+#NOT(!)
+!FALSE
+!TRUE
+!x
+
