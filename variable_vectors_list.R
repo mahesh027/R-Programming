4bae684686476cc5f3e2877a4bc7da8e2f3b6228
K Mahesh <mahesh0352002@gmail.com>
2023-05-16 08:33:41 +0530

    vectors

--- variable_vectors_list.R
+++ variable_vectors_list.R
@@ -1,3 +1,34 @@
 first<-5.5
 first
+second<-"this is variable"
+second
+#vectors
+vec_1<-c(13,48.5,71,10.5,6)
+vec_1
+vec_2<-c(1L,2L,15L) #L represent integers
+vec_3<-c("sara","sachin","gill")
+vec_4<-c(TRUE,FALSE,TRUE)
+vec_2
+vec_3
+vec_4
+
+#properties of vectors
+typeof(vec_1)
+typeof(vec_2)
+typeof(vec_3)
+typeof(vec_4)
+length(vec_1)
+length(vec_2)
+length(vec_3)
+length(vec_4)
+is.integer(vec_1)
+is.integer(vec_2)
+is.logical(vec_4)
+is.character(vec_3)
+is.double(vec_1)
+
+#naming vectors by names() function
+x<-c(1,3,5)
+names(x)<-c("a","b","c")
+
 
