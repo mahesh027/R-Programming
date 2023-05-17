be5738b658a89fab235783dd74b634a606b5688a
K Mahesh <mahesh0352002@gmail.com>
2023-05-17 12:24:46 +0530

    commit

--- ggplot_basic.R
+++ ggplot_basic.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(palmerpenguins)
+#penguin dataset
 ggplot(data=penguins)+geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g))
 ggplot(data=penguins)+geom_point(mapping=aes(x=bill_length_mm,y=bill_depth_mm))
 ggplot(data=penguins)+geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))
@@ -10,4 +11,18 @@ ggplot(data=penguins)+geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,a
 ggplot(data=penguins)+geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g),color="purple")
 
 
-#run this code line by line to know the difference of each function
+#geom_function
+ggplot(data=penguins)+geom_smooth(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+ggplot(data=penguins)+geom_smooth(mapping=aes(x=flipper_length_mm,y=body_mass_g))+geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+ggplot(data=penguins)+geom_smooth(mapping=aes(x=flipper_length_mm,y=body_mass_g,linetype=species))
+ggplot(data=penguins)+geom_jitter(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+
+
+#diamonds-dataset
+ggplot(data=diamonds)+geom_bar(mapping=aes(x=cut)) #cut is an attribute in diamonds dataset,y axis automatically set as count
+ggplot(data=diamonds)+geom_bar(mapping=aes(x=cut,color=cut))
+ggplot(data=diamonds)+geom_bar(mapping=aes(x=cut,fill=cut))
+ggplot(data=diamonds)+geom_bar(mapping=aes(x=cut,fill=clarity)) #clarity is another attribut,check diamond dataset
+
+
+#run these code line by line to know the difference of each function
